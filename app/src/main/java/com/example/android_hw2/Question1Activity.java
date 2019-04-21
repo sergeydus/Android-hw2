a2224e6b97e90b38da226e0842e779aefc5b67cd
alexdushevski <39350701+Tehheavy@users.noreply.github.com>
2019-04-21 21:44:08 +0300

    #1 Added working scorebar(result currently shown in Question 2)
    #1

--- app/src/main/java/com/example/android_hw2/Question1Activity.java
+++ app/src/main/java/com/example/android_hw2/Question1Activity.java
@@ -1,13 +1,71 @@
 package com.example.android_hw2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Question1Activity extends AppCompatActivity {
-
+    private Button Answer1Button;
+    private Button Answer2Button;
+    private Button Answer3Button;
+    private Button Answer4Button;
+    int StartingValue=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question1);
+
+        Answer1Button= (Button) findViewById(R.id.Q1button1);
+        Answer1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+        Answer2Button= (Button) findViewById(R.id.Q1button2);
+        Answer2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+        Answer3Button= (Button) findViewById(R.id.Q1button3);
+        Answer3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+        Answer4Button= (Button) findViewById(R.id.Q1button4);
+        Answer4Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity4();
+            }
+        });
+    }
+
+
+    public void openActivity1(){
+        Intent intent = new Intent(this, Question2Activity.class);
+        intent.putExtra("score",StartingValue+1);
+        startActivity(intent);
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, Question2Activity.class);
+        intent.putExtra("score",StartingValue+0);
+        startActivity(intent);
+    }
+    public void openActivity3(){
+        Intent intent = new Intent(this, Question2Activity.class);
+        intent.putExtra("score",StartingValue+0);
+        startActivity(intent);
+    }
+    public void openActivity4(){
+        Intent intent = new Intent(this, Question2Activity.class);
+        intent.putExtra("score",StartingValue+0);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/android_hw2/Question2Activity.java
+++ app/src/main/java/com/example/android_hw2/Question2Activity.java
@@ -2,12 +2,19 @@ package com.example.android_hw2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Question2Activity extends AppCompatActivity {
 
+    TextView test;
+    int currentScore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question2);
+        currentScore=getIntent().getIntExtra("score",0);
+
+        test=findViewById(R.id.Q2TextView);
+        test.setText("the current score is "+currentScore);
     }
 }
