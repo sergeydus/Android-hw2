63b0969c3b46c5869df3f3ce340e695aa611f798
alexdushevski <39350701+Tehheavy@users.noreply.github.com>
2019-04-21 03:59:04 +0300

    Added empty files (empty activities 1-10 and result screen)
    yes

---
+++ app/src/main/java/com/example/android_hw2/Question10Activity.java
@@ -0,0 +1,13 @@
+package com.example.android_hw2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Question10Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question10);
+    }
+}
