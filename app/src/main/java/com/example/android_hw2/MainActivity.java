fa771699deff9802e6eb2b93f47a42b9e0871d15
alexdushevski <39350701+Tehheavy@users.noreply.github.com>
2019-04-21 21:21:48 +0300

    #1 added some of question 1 , and added home page
    #1 yes

--- app/src/main/java/com/example/android_hw2/MainActivity.java
+++ app/src/main/java/com/example/android_hw2/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.android_hw2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button ButtonStartQuiz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButtonStartQuiz= (Button) findViewById(R.id.ButtonStart);
+        ButtonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFirstQuestionActivity();
+            }
+        });
+    }
+    public void openFirstQuestionActivity(){
+        Intent intent = new Intent(this, Question1Activity.class);
+        // intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY); // Adds the FLAG_ACTIVITY_NO_HISTORY flag
+        startActivity(intent);
     }
 }
