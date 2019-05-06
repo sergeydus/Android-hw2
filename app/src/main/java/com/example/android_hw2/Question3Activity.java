d9148ef4a6b80d416b1c20c112c8e6caa8a78db7
EricFreeman <gogosergey@gmail.com>
2019-05-06 14:02:18 +0300

    Added page 3
    added page 3 and from page 1 we move to 3 then to result page

--- app/src/main/java/com/example/android_hw2/Question3Activity.java
+++ app/src/main/java/com/example/android_hw2/Question3Activity.java
@@ -1,13 +1,72 @@
 package com.example.android_hw2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Question3Activity extends AppCompatActivity {
-
+    private Button Answer1Button;
+    private Button Answer2Button;
+    private Button Answer3Button;
+    private Button Answer4Button;
+    int currentScore=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question3);
+        currentScore=getIntent().getIntExtra("score",0);
+
+        Answer1Button= (Button) findViewById(R.id.Q3button1);
+        Answer1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+        Answer2Button= (Button) findViewById(R.id.Q3button2);
+        Answer2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+        Answer3Button= (Button) findViewById(R.id.Q3button3);
+        Answer3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+        Answer4Button= (Button) findViewById(R.id.Q3button4);
+        Answer4Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity4();
+            }
+        });
+    }
+
+
+    public void openActivity1(){
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("score",currentScore+0);
+        startActivity(intent);
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("score",currentScore+1);
+        startActivity(intent);
+    }
+    public void openActivity3(){
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("score",currentScore+0);
+        startActivity(intent);
+    }
+    public void openActivity4(){
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("score",currentScore+0);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/android_hw2/ResultActivity.java
+++ app/src/main/java/com/example/android_hw2/ResultActivity.java
@@ -2,12 +2,19 @@ package com.example.android_hw2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
+    TextView test;
+    int currentScore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        currentScore=getIntent().getIntExtra("score",0);
+
+        test=findViewById(R.id.ResultTextView);
+        test.setText("the current score is "+currentScore);
     }
 }
