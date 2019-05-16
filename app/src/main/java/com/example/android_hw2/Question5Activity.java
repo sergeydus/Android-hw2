b52ae686a7f7917b996d4101733c5373fabb1980
levyron91 <48878494+levyron91@users.noreply.github.com>
2019-05-16 11:05:23 +0300

    #3
    Question 5 & 6 ready and checked, images updated, and java and xml ready

--- app/src/main/java/com/example/android_hw2/Question5Activity.java
+++ app/src/main/java/com/example/android_hw2/Question5Activity.java
@@ -1,13 +1,72 @@
 package com.example.android_hw2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Question5Activity extends AppCompatActivity {
 
+    private Button Answer1Button;
+    private Button Answer2Button;
+    private Button Answer3Button;
+    private Button Answer4Button;
+    int currentScore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question5);
+        currentScore=getIntent().getIntExtra("score",0);
+        Answer1Button= (Button) findViewById(R.id.Q5button1);
+        Answer1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+        Answer2Button= (Button) findViewById(R.id.Q5button2);
+        Answer2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+        Answer3Button= (Button) findViewById(R.id.Q5button3);
+        Answer3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+        Answer4Button= (Button) findViewById(R.id.Q5button4);
+        Answer4Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity4();
+            }
+        });
+    }
+
+
+    public void openActivity1(){
+        Intent intent = new Intent(this, Question6Activity.class);
+        intent.putExtra("score",currentScore+0);
+        startActivity(intent);
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, Question6Activity.class);
+        intent.putExtra("score",currentScore+0);
+        startActivity(intent);
+    }
+    public void openActivity3(){
+        Intent intent = new Intent(this, Question6Activity.class);
+        intent.putExtra("score",currentScore+0);
+        startActivity(intent);
+    }
+    public void openActivity4(){
+        Intent intent = new Intent(this, Question6Activity.class);
+        intent.putExtra("score",currentScore+1);
+        startActivity(intent);
     }
 }
